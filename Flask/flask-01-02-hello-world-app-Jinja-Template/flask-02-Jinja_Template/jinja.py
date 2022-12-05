36ccc06d785154a309a6951f27e122a0b87631dc
OomerFarukk <omerfaruksahin12@gmail.com>
2022-12-05 23:33:40 +0300

    new

--- Flask/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
+++ Flask/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def head():
     return render_template('index.html', number1 = 117000, number2 = 229000)
 
-@app.route('/mult')
+@app.route('/sum')
 def number():
     var1, var2 = 15210, 38960
-    return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1*var2)
+    return render_template('body.html', value1 = var1, value2 = var2, sum = var1+var2)
 
 
 if __name__ == '__main__':
